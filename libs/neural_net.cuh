89040c78c75767a6cbf67aaf01106d917d08cf07
s332958 <s332958@studenti.polito.it>
2025-05-15 18:06:05 +0200

    AGGIUNTA CARTELLA CON I NUOVI FILE (FASE 1 NON COMPLETA)

--- libs/neural_net.cuh
+++ libs/neural_net.cuh
@@ -1,4 +1,5 @@
 #pragma once
+#include <cuda_runtime.h>
 
 struct NeuralNet {
     int numLayers;
@@ -18,3 +19,12 @@ struct NeuralNet {
     void forwardOnDevice(const float* h_input, float* h_output);
     
 };
+
+
+void argsort_bubble(int *vettore, int *indice, int n);
+void argsort_bubble(float *vettore, int *indice, int n);
+
+void wrapper_recombination(NeuralNet *neuralNets, NeuralNet *newNeuralNet, int totNeuralNet, float *total_energy, int *total_coverage, float limit, 
+    int type_of_union, int random_mutation_x_block, int dim_block, float max_varaiation_mutation,cudaStream_t stream);
+
+__global__ void recombination(NeuralNet n1, NeuralNet n2, NeuralNet final, int n_random_mutation_x_block, float max_value_mutation, int totalWeights, int totalBiases);
